d99003950e99edca3d1fb7ac81d02e17e0f2ec3d
Pujan <37817715+heisenbug-io@users.noreply.github.com>
2024-06-07 19:06:48 -0400

    Camp section

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
         <Image src="menu.svg" alt="menu" width={32} height={32} className="inline-block cursor-pointer lg:hidden" />
 
-        // TODO - Add mobile menu
+        {/* TODO - Add mobile menu */}
        
     </nav>
   )
