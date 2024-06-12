649861e82caa700ad083faf54917a60ed00357c1
Pujan <37817715+heisenbug-io@users.noreply.github.com>
2024-06-12 16:51:11 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV = 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
+    assetPrefix: isProd ? 'https://heisenbug-io.github.io/nextjs-travel-app-landing-page' : undefined,
     output: 'export',
     images: { unoptimized: true },
     basePath: isProd ? '/nextjs-travel-app-landing-page' : '',
