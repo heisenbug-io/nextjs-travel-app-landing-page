87a19623c5b28e2db6939062fee7161cbf35836e
Pujan <37817715+heisenbug-io@users.noreply.github.com>
2024-06-12 17:00:02 -0400

    adding prefix for images

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,13 @@ import Link from "next/link"
 import Image from "next/image"
 import { NAV_LINKS } from "@/constants"
 import Button from "@/components/Button"
+import { prefix } from "./prefix"
 
 const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
         <Link href="/">
-            <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+            <Image src={`${prefix}/hilink-logo.svg`} alt="logo" width={74} height={29} />
          </Link>
         <ul className="hidden h-full gap-12 lg:flex">
             {NAV_LINKS.map((link) => (
